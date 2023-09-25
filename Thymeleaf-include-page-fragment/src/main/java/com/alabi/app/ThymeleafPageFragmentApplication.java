907f0cc4b22aa1c62767c94b3c81e941d9a6a403
Razaq <captain.alabi@gmail.com>
2023-09-25 11:24:46 +0100

    Add files via upload

---
+++ Thymeleaf-include-page-fragment/src/main/java/com/alabi/app/ThymeleafPageFragmentApplication.java
@@ -0,0 +1,13 @@
+package com.alabi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPageFragmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPageFragmentApplication.class, args);
+	}
+
+}
